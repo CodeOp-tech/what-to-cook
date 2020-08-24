7b68d8d248cfbb3d23cd3f2b793b750558111555
Irina <irinasensei@gmail.com>
2020-08-24 17:47:12 +0900

    irina probably didn't change anything

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Search from "./components/search";
+import Search from "./components/Search";
 import { BrowserRouter, Link, NavLink, Route, Switch } from "react-router-dom";
 import Navbar from "./components/NavBar";
 import Routes from "./components/Routes";
