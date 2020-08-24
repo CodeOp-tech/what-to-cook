c821bbb2e54d2350367f5cc880271d026c64e866
Iva Bozic <ivabozic23@gmail.com>
2020-08-24 10:41:46 +0100

    trying out

--- client/src/components/search.js
+++ client/src/components/search.js
@@ -43,36 +43,74 @@ export default class Search extends Component {
       ingredients: e.target.value,
     });
   };
-    render() {
-        const { ingredients, recipes, loading } = this.state;
-        return (
-            <div className="col input-group-lg">
-                WHAT TO COOK..
-                <form className="search-form">
-                    <input
-                        type="text"
-                        value={ingredients}
-                        onChange={this.handleInput}
-                        className="form-control mb-1"
-                        maxLength="50"
-                        placeholder="Find a recipe by adding ingredients.."
-                    ></input>
-                    
-                    <button 
-                        className="btn btn-outline-secondary" 
-                        onClick={this.searchRecipes}>
-                        search
-                    </button> 
-                    
-                    <div>
-                        {loading ? <span>Loading...</span> : null}
-                        {recipes.length > 0 
-                            ?  <Redirect to={{ pathname:"/recipe", state: { recipes: recipes }}} /> 
-                            :  <Redirect to="/" /> } {/* TODO if response empty what? Stay in current location? */}
-                    </div>
-                </form>
-            </div>
-        )
-    }
+  render() {
+    const { ingredients, recipes, loading } = this.state;
+    return (
+      <div className="col input-group-lg">
+        WHAT TO COOK..
+        <form className="search-form">
+          <input
+            type="text"
+            value={ingredients}
+            onChange={this.handleInput}
+            className="form-control mb-1"
+            maxLength="50"
+            placeholder="Find a recipe by adding ingredients.."
+          ></input>
 
+          <button
+            className="btn btn-outline-secondary"
+            onClick={this.searchRecipes}
+          >
+            search
+          </button>
+
+          <div>
+            {loading ? <span>Loading...</span> : null}
+            {recipes.length > 0 ? (
+              <Redirect
+                to={{ pathname: "/recipe", state: { recipes: recipes } }}
+              />
+            ) : (
+              <Redirect to="/" />
+            )}{" "}
+            {/* TODO if response empty what? Stay in current location? */}
+          </div>
+        </form>
+      </div>
+    );
+  }
+
+  render() {
+    const { ingredients, recipes, loading } = this.state;
+    return (
+      <div className="col input-group-lg">
+        WHAT TO COOK..
+        <form className="search-form">
+          <input
+            type="text"
+            value={ingredients}
+            onChange={this.handleInput}
+            className="form-control mb-1"
+            maxLength="50"
+            placeholder="Find a recipe by adding ingredients.."
+          ></input>
+
+          <button
+            className="btn btn-outline-secondary"
+            onClick={this.searchRecipes}
+          >
+            search
+          </button>
+
+          <div>
+            {loading ? <span>Loading...</span> : null}
+            {recipes.map((recipe) => (
+              <RecipeSearchItem image={recipe.image} title={recipe.title} />
+            ))}
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
