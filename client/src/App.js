2055c8efb48cad47e3602af1fde8599d5af7d32a
Irina <irinasensei@gmail.com>
2020-08-27 18:11:58 +0900

    homepage styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,8 @@
 import React from "react";
-
 import Search from "./components/search";
 import { BrowserRouter, Switch } from "react-router-dom";
-
 import Navbar from "./components/NavBar";
 import Routes from "./components/Routes";
-
 import "./App.css";
 
 class App extends React.Component {
@@ -13,7 +10,6 @@ class App extends React.Component {
     super(props);
     this.state = {
       userId: 4,
-      userLoggedIn: 1,
     };
   }
   //logout button pressed
@@ -25,35 +21,37 @@ class App extends React.Component {
       userLoggedIn: 0,
     });
   };
+
   //comment
   render() {
     const { userLoggedIn } = this.state;
     return (
       <div className="App">
-        <header>
-          <nav>
-            <h1 className="fixed-top"> WHAT TO COOK</h1>
-            <BrowserRouter>
-              <Navbar />
-
-              <Search />
-
+        <div className="container-fluid">
+          <BrowserRouter>
+            <div>
+              <header className="row">
+                <h1 className="col"> WHAT TO COOK</h1>
+                <div className="col">
+                  <Navbar />
+                </div>
+              </header>
+            </div>
+            <main className="row" id="mainbg">
               <Routes />
               <Switch />
-            </BrowserRouter>
-          </nav>
-        </header>
-        <main className="container m-5 p-5">
-          This is the area for the main content
-        </main>
-        <footer>
-          this the footer which doesnt exist in design but we probably should
-          write somethings like:<br></br>
-          <bold>
-            Made with &hearts; by Iva Bozic, Julieta Martin, Irina Eliseeva as a
-            collaboration project for Codeop bootcamp, FS-09{" "}
-          </bold>
-        </footer>
+              This is the area for the main content
+            </main>
+            <footer className="row">
+              this the footer which doesnt exist in design but we probably
+              should write somethings like:<br></br>
+              <strong>
+                Made with &hearts; by Iva Bozic, Julieta Martin, Irina Eliseeva
+                as a collaboration project for Codeop bootcamp, FS-09{" "}
+              </strong>
+            </footer>
+          </BrowserRouter>
+        </div>
       </div>
     );
   }
--- client/src/components/HomeView.js
+++ client/src/components/HomeView.js
@@ -4,7 +4,39 @@ export default class HomeView extends Component {
   render() {
     return (
       <div>
-        <h2>this is homeview</h2>
+        <div id="banneronthetop">
+          this is homeview
+          <div className="container" id="textinbanner">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud
+          </div>
+        </div>
+        <div class="card-deck">
+          <div class="card">
+            <img class="card-img-top" src="images/cake.jpg" alt="cake" />
+
+            <div class="card-body text-center">
+              <h4>Recipe</h4>
+              <p class="card-text">Some text inside the first card</p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body text-center">
+              <p class="card-text">Some text inside the second card</p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body text-center">
+              <p class="card-text">Some text inside the third card</p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body text-center">
+              <p class="card-text">Some text inside the fourth card</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
