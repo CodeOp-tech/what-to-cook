0f7f21937bf8c281264c343ae30ed9e8dbfcd97b
Iva Bozic <ivabozic23@gmail.com>
2020-08-27 13:18:56 +0100

    detail.js checks favourite

--- client/src/components/Detail.js
+++ client/src/components/Detail.js
@@ -1,42 +1,118 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { decodeBase64 } from "bcryptjs";
 
 const RECIPE_API_KEY = process.env.REACT_APP_RECIPE_API_KEY;
 
 export default class Detail extends Component {
-    state = {
-        recipe: null
-    }
-    
-    componentDidMount(){
-        const { id } = this.props.match.params
-        fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${RECIPE_API_KEY}`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-        .then(res => res.json())
-        .then(res => 
-            this.setState({ recipe: res })
-        )
-        .catch(err => { console.log(err)})
-    }
+  state = {
+    recipe: null,
+    isFavourite: 1,
+  };
 
-    render() {
-        const { recipe } = this.state
-        return (
-            <div>
-                {!recipe 
-                    ? <span>loading...</span>
-                    : (
-                        <div>
-                            <h3>{recipe.title}</h3>
-                            <img alt={recipe.title} src={recipe.image} width="100" height="100" />                    
-                            <div>{recipe.instructions}</div>
-                        </div>
-                    )
-                }
-            </div>
-        )
+  //add recipe to favourites
+  addToFavourites = async () => {
+    console.log("adding recipe to favourites");
+    const recipeId = { recipeId: this.props.match.params.id };
+    try {
+      await fetch("/favourites/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "x-access-token": localStorage.getItem("token"),
+        },
+        body: JSON.stringify(recipeId),
+      });
+    } catch (err) {
+      console.log(err);
     }
+    //fetch request to add user-recipe link
+    this.setState({
+      isFavourite: 1,
+    });
+  };
+
+  //remove recipe from favourites
+  removeFromFavourites = () => {
+    //fetch request to remove user-recipe link
+
+    console.log("removing recipe from favourites");
+    this.setState({
+      isFavourite: 0,
+    });
+  };
+
+  //check if recipe is already in favourites
+  isInFavourites = async () => {
+    const { id } = this.props.match.params;
+    //check if recipe in favourites
+    //if it is, change state isFavourite 1
+    try {
+      const result = await fetch(`/api/favourites/${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "x-access-token": localStorage.getItem("token"),
+        },
+      });
+      const json = await result.json();
+      //   console.log(json);
+      //if the response is not negative meaning a match was found
+      if (json.msg !== "Not a favourite") {
+        this.setState({ isFavourite: 1 });
+      } else {
+        this.setState({ isFavourite: 0 });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    fetch(
+      `https://api.spoonacular.com/recipes/${id}/information?apiKey=${RECIPE_API_KEY}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((res) => res.json())
+      .then((res) => this.setState({ recipe: res }))
+      .catch((err) => {
+        console.log(err);
+      });
+    //check if the recipe shown is already a favourite
+    this.isInFavourites();
+  }
+
+  render() {
+    const { recipe, isFavourite } = this.state;
+    return (
+      <div>
+        {!recipe ? (
+          <span>loading...</span>
+        ) : (
+          <div>
+            <h3>
+              {recipe.title}{" "}
+              {isFavourite ? (
+                <i class="fas fa-star" onClick={this.removeFromFavourites}></i>
+              ) : (
+                <i className="far fa-star" onClick={this.addToFavourites}></i>
+              )}
+            </h3>{" "}
+            <img
+              alt={recipe.title}
+              src={recipe.image}
+              width="100"
+              height="100"
+            />
+            <div>{recipe.instructions}</div>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
--- routes/api.js
+++ routes/api.js
@@ -90,11 +90,27 @@ router.post("/favourites", isUserLoggedIn, async (req, res) => {
   }
 });
 
-//GET favourite recipes for logged in user
-router.get("/favourites", isUserLoggedIn, async (req, res) => {
+// GET favourite recipes for logged in user
+router.get("/favourites/all", isUserLoggedIn, async (req, res) => {
   try {
     results = await db(`select * from favourites where userId='${req.userId}'`);
     res.status(200).send(results.data);
+  } catch (err) {
+    res.status(400).send({ msg: err });
+  }
+});
+
+//check if recipe is favourite for logged in user
+router.get("/favourites/:recipeId", isUserLoggedIn, async (req, res) => {
+  try {
+    results = await db(
+      `select * from favourites where userId='${req.userId}' and recipeId ='${req.params.recipeId};'`
+    );
+    if (results.data.length) {
+      res.status(200).send(results.data);
+    } else {
+      res.status(200).send({ msg: "Not a favourite" });
+    }
   } catch (err) {
     res.status(400).send({ msg: err });
   }
