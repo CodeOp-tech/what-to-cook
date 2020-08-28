e7bccf053f09ae838053b9e9c5295405b8d83438
Irina <irinasensei@gmail.com>
2020-08-28 16:40:27 +0900

    nabvar and cards in progress

--- client/src/components/HomeView.js
+++ client/src/components/HomeView.js
@@ -3,37 +3,101 @@ import React, { Component } from "react";
 export default class HomeView extends Component {
   render() {
     return (
-      <div>
-        <div id="banneronthetop">
-          this is homeview
-          <div className="container" id="textinbanner">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col">
+            <section className="bgimage">
+              <div class="container-fluid">
+                <div class="row">
+                  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <h3>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam, quis nostrud
+                    </h3>
+                  </div>
+                </div>
+              </div>
+            </section>
           </div>
         </div>
-        <div class="card-deck">
-          <div class="card">
-            <img class="card-img-top" src="images/cake.jpg" alt="cake" />
+        <div className="Carousel mt-4">
+          <div className="carousel slide" data-interval="2000">
+            <h2 className="text-center">slide 1</h2>
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <div className="display-4">
+                  <div className="card-deck">
+                    <div className="card">
+                      <img
+                        className="card-img-top"
+                        src="https://images.unsplash.com/photo-1559620192-032c4bc4674e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=970&q=80"
+                        alt="cake"
+                      />
 
-            <div class="card-body text-center">
-              <h4>Recipe</h4>
-              <p class="card-text">Some text inside the first card</p>
-            </div>
-          </div>
-          <div class="card">
-            <div class="card-body text-center">
-              <p class="card-text">Some text inside the second card</p>
-            </div>
-          </div>
-          <div class="card">
-            <div class="card-body text-center">
-              <p class="card-text">Some text inside the third card</p>
-            </div>
-          </div>
-          <div class="card">
-            <div class="card-body text-center">
-              <p class="card-text">Some text inside the fourth card</p>
+                      <div className="card-body text-center">
+                        <h4>Recipe card 1</h4>
+                        <p className="card-text">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit, sed do
+                        </p>
+                      </div>
+                    </div>
+                    <div className="card">
+                      <img
+                        className="card-img-top"
+                        src="https://images.unsplash.com/photo-1559620192-032c4bc4674e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=970&q=80"
+                        alt="cake"
+                      />
+                      <div className="card-body text-center">
+                        <h4>Recipe card 2</h4>
+                        <p className="card-text">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit, sed do
+                        </p>
+                      </div>
+                    </div>
+                    <div className="card">
+                      <img
+                        className="card-img-top"
+                        src="https://images.unsplash.com/photo-1559620192-032c4bc4674e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=970&q=80"
+                        alt="cake"
+                      />
+                      <div className="card-body text-center">
+                        <h4>Recipe card 3</h4>
+                        <p className="card-text">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit, sed do
+                        </p>
+                      </div>
+                    </div>
+                    <div className="card">
+                      <img
+                        className="card-img-top"
+                        src="https://images.unsplash.com/photo-1559620192-032c4bc4674e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=970&q=80"
+                        alt="cake"
+                      />
+                      <div className="card-body text-center">
+                        <h4>Recipe card 4</h4>
+                        <p className="card-text">
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit, sed do
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="carousel-item">
+                    <div className="display-4">slide 2</div>
+                  </div>
+
+                  <div className="carousel-item">
+                    <div className="display-4">slide 3</div>
+                  </div>
+                </div>
+              </div>
+              <button>
+                <i className="fas fa-chevron-circle-right"></i>
+              </button>
             </div>
           </div>
         </div>
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import Search from "./search";
+import "./NavBar.css";
 
 export default class NavBar extends Component {
   constructor(props) {
@@ -16,46 +17,52 @@ export default class NavBar extends Component {
   render() {
     const { userLoggedIn } = this.props;
     return (
-      <nav>
-        <ul>
-          <li>
-            <Search />
-          </li>
-          <li>
-            <NavLink to="/" exact activeClassName="selected">
-              Home
-            </NavLink>
-          </li>
-          {userLoggedIn ? null : (
-            <li>
-              <NavLink to="/create" activeClassName="selected">
-                Create account
-              </NavLink>
-            </li>
-          )}
+      <div className="Navbar">
+        <nav className="navbar navbar-expand-md">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-          {userLoggedIn ? null : (
-            <li>
-              <NavLink to="/login" activeClassName="selected">
-                Login
-              </NavLink>
-            </li>
-          )}
+          <div className="collapse navbar-collapse"></div>
+          <ul className="navbar-nav">
+            {userLoggedIn ? null : (
+              <li className="nav-item active">
+                <NavLink to="/create" activeClassName="selected">
+                  Create account
+                </NavLink>
+              </li>
+            )}
 
-          {userLoggedIn ? (
-            <li>
-              <NavLink to="/favourites" activeClassName="selected">
-                Favourites
-              </NavLink>
-            </li>
-          ) : null}
-          {userLoggedIn ? (
-            <li onClick={this.onLogoutPress} id="logout">
-              Logout
-            </li>
-          ) : null}
-        </ul>
-      </nav>
+            {userLoggedIn ? null : (
+              <li className="nav-item active">
+                <NavLink to="/login" activeClassName="selected">
+                  Login
+                </NavLink>
+              </li>
+            )}
+
+            {userLoggedIn ? (
+              <li className="nav-item active">
+                <NavLink to="/favourites" activeClassName="selected">
+                  Favourites
+                </NavLink>
+              </li>
+            ) : null}
+            {userLoggedIn ? (
+              <li onClick={this.onLogoutPress} id="logout">
+                Logout
+              </li>
+            ) : null}
+          </ul>
+        </nav>
+      </div>
     );
   }
 }
