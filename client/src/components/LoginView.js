2352cf51088a15b5235499f939db713e674e2174
Iva Bozic <ivabozic23@gmail.com>
2020-08-31 12:19:41 +0100

    changed placeholder colors for create and login

--- client/src/components/LoginView.js
+++ client/src/components/LoginView.js
@@ -11,7 +11,7 @@ export default class LoginView extends Component {
       },
       userLoggedIn: 0,
       userLoginError: 0,
-      errorMessage: "",
+      errorMessage: "Incorrect login",
     };
   }
   handleInputChange = (e) => {
@@ -60,45 +60,62 @@ export default class LoginView extends Component {
 
   render() {
     const { username, password } = this.state.existingUser;
-    const { userLoginError, userLoggedIn } = this.state;
+    const { userLoginError, errorMessage } = this.state;
     return (
       <React.Fragment>
-       <div className="col-sm-6 text-center d-flex justify-content-center align-items-center">
-         <div className="box">
-          <h1 className="mb-4" id="titleLogin">Log In</h1>
-          <form onSubmit={this.handleSubmit} className="inputs d-inline-block text-center" id="form-login"> 
-           <input
-              type="text"
-              name="username"
-              value={username}
-              placeholder="username"
-              onChange={this.handleInputChange}
-              required
-              pattern="[A-Za-z0-9]{2,}"
-              className="form-control-lg mb-4"
-            ></input>
-            <input
-              type="password"
-              name="password"
-              value={password}
-              placeholder="password"
-              onChange={this.handleInputChange}
-              required
-              className="form-control-lg mb-4"
-            ></input>
-            <button type="sumbit" className="form-control-lg" id="buttonLogin">Submit</button>
-          </form>
+        <div className="col-sm-6 text-center d-flex justify-content-center align-items-center">
+          <div className="box">
+            <h1 className="mb-4" id="titleLogin">
+              Log In
+            </h1>
+            <form
+              onSubmit={this.handleSubmit}
+              className="inputs d-inline-block text-center"
+              id="form-login"
+            >
+              <input
+                type="text"
+                name="username"
+                value={username}
+                placeholder="Username..."
+                onChange={this.handleInputChange}
+                required
+                pattern="[A-Za-z0-9]{2,}"
+                className="form-control-lg mb-4 custom-input-placeholder"
+              ></input>
+              <input
+                type="password"
+                name="password"
+                value={password}
+                placeholder="Password..."
+                onChange={this.handleInputChange}
+                required
+                className="form-control-lg mb-4 custom-input-placeholder"
+              ></input>
+              <button
+                type="sumbit"
+                className="form-control-lg"
+                id="buttonLogin"
+              >
+                Submit
+              </button>
+              {userLoginError ? <p>{errorMessage}</p> : null}
+            </form>
           </div>
         </div>
         <div id="f1_container">
-          <div id="f1_card" >
+          <div id="f1_card">
             <div className="col-sm-6">
-              <img src="/images/pizza.jpg" alt="pizza" width="500" className="rounded-lg mx-auto d-block flip-box-back" id="imagelogin"/>
+              <img
+                src="/images/pizza.jpg"
+                alt="pizza"
+                width="500"
+                className="rounded-lg mx-auto d-block flip-box-back"
+                id="imagelogin"
+              />
             </div>
           </div>
         </div>
-    
-    
       </React.Fragment>
     );
   }
